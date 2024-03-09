9689c37cbf8d9fab050089cdd9e9960e24927ed5
Almost-Senseless-Coder <109462027+Almost-Senseless-Coder@users.noreply.github.com>
2024-03-09 13:23:53 +0100

    Added a back navigation button to the OSSLicensesActivity (#24)
    Resolves #23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui:1.6.2")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
--- app/src/main/java/tech/almost_senseless/voskle/OSSLicensesActivity.kt
+++ app/src/main/java/tech/almost_senseless/voskle/OSSLicensesActivity.kt
@@ -3,14 +3,22 @@ package tech.almost_senseless.voskle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
@@ -20,28 +28,51 @@ import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import tech.almost_senseless.voskle.ui.theme.VoskleLiveTranscribeTheme
 
 class OSSLicensesActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             VoskleLiveTranscribeTheme(darkTheme = isSystemInDarkTheme(), dynamicColor = true) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
+                val context = LocalContext.current
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(text = stringResource(id = R.string.oss_licenses), fontSize = 5.em,
-                            modifier = Modifier
-                                .semantics { heading() },
-                            fontWeight = FontWeight.Bold
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                    text = stringResource(id = R.string.oss_licenses),
+                                        fontSize = 5.em,
+                                        modifier = Modifier
+                                            .semantics { heading() },
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                },
+                                navigationIcon = {
+                                    IconButton(
+                                        onClick = {
+                                            finish()
+                                        }
+                                    ) {
+                                        Icon(
+                                            imageVector = Icons.Filled.ArrowBack,
+                                            contentDescription = context.getString(R.string.back)
+                                        )
+                                    }
+                                },
+                                colors = TopAppBarDefaults.mediumTopAppBarColors()
+                            )
+                        }
+                    ) { contentPadding ->
+                        LibrariesContainer(
+                            Modifier
+                                .fillMaxSize()
+                                .padding(contentPadding)
                         )
                     }
-                    LibrariesContainer(
-                        Modifier.fillMaxSize()
-                    )
                 }
             }
         }
